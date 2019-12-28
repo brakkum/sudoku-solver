94d2145e7d3ab302fc3dbc03b3ad85239872ccc4
Daniel Brakke <brakphoto@gmail.com>
2019-12-27 20:54:31 -0600

    new board

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -16,17 +16,17 @@ if __name__ == "__main__":
         [None, None, None, None, None, None, None, None, None]
     """
     board = [
-        [None, None, None, None, None, None, None, None, None],
-        [None, None, None, None, None, None, None, None, None],
-        [None, None, None, None, None, None, None, None, None],
+        [None,    8, None,    3, None, None, None, None,    7],
+        [None, None,    2, None,    1, None, None, None,    6],
+        [   7, None,    3,    6,    5, None, None, None, None],
         ######################################################
-        [None, None, None, None, None, None, None, None, None],
-        [None, None, None, None, None, None, None, None, None],
-        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None,    8, None,    2, None],
+        [   4, None,    8, None, None, None,    7, None,    1],
+        [None,    2, None,    1, None, None, None, None, None],
         ######################################################
-        [None, None, None, None, None, None, None, None, None],
-        [None, None, None, None, None, None, None, None, None],
-        [None, None, None, None, None, None, None, None, None]
+        [None, None, None, None,    8,    2,    6, None,    4],
+        [   8, None, None, None,    4, None,    5, None, None],
+        [   2, None, None, None, None,    6, None,    9, None]
     ]
     sudoku = Sudoku(board)
     sudoku.populate_initial_board()
